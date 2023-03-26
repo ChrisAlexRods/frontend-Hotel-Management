a600feb7dc682206932f2d4fe6deda87074f88b8
Christian Rodriguez <105233007+ChrisAlexRods@users.noreply.github.com>
2023-03-26 08:56:26 -0400

    Images Section

--- src/HomePage.js
+++ src/HomePage.js
@@ -84,13 +84,37 @@ const HomePage = () => {
       </section>
 
       <section className={styles.images}>
-        <div className="grid">
-          <img src="image1.jpg" alt="Image 1" />
-          <img src="image2.jpg" alt="Image 2" />
-          <img src="image3.jpg" alt="Image 3" />
-          <img src="image4.jpg" alt="Image 4" />
+        <div className={styles.wrapper}>
+          <div className={styles.stripe}>Always there with just what you need</div>
+          <div className={styles.grid}>
+            <div className={styles.card}>
+              <img src={`${process.env.PUBLIC_URL}/4.jpg`} alt="Image 4" />
+              <div className={styles.caption}>Enjoy our dedicated office space</div>
+            </div>
+            <div className={styles.card}>
+              <img src={`${process.env.PUBLIC_URL}/5.jpg`} alt="Image 5" />
+              <div className={styles.caption}>Stay and rest in our sauna</div>
+            </div>
+            <div className={styles.card}>
+              <img src={`${process.env.PUBLIC_URL}/6.jpg`} alt="Image 6" />
+              <div className={styles.caption}>Take dive into our traditional rooms</div>
+            </div>
+            <div className={styles.card}>
+              <img src={`${process.env.PUBLIC_URL}/13.jpg`} alt="Image 13" />
+              <div className={styles.caption}>Enjoy our masterclass bedrooms</div>
+            </div>
+            <div className={styles.card}>
+              <img src={`${process.env.PUBLIC_URL}/14.jpg`} alt="Image 14" />
+              <div className={styles.caption}>Enjoy our open bar</div>
+            </div>
+            <div className={styles.card}>
+              <img src={`${process.env.PUBLIC_URL}/15.jpg`} alt="Image 15" />
+              <div className={styles.caption}>Rest easy in our lounge</div>
+            </div>
+          </div>
         </div>
       </section>
+
 
       <section className={styles.description}>
         <p>
