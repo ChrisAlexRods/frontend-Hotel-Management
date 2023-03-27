a81437fcf0e6662a0dd7bdde93d5887ff5499120
Christian Rodriguez <105233007+ChrisAlexRods@users.noreply.github.com>
2023-03-27 17:36:28 -0400

    Started menu section

--- src/HomePage.js
+++ src/HomePage.js
@@ -26,13 +26,41 @@ const HomePage = () => {
   const menu = [
     {
       category: 'STARTERS', items: [
-        { name: 'Old Fashioned Oatmeal', price: 14 },
-        { name: 'Fresh Yogurt & Maple Granola', price: 13, description: '(Organic Berries, Raw New York Honey)' },
+        { name: 'Old Fashioned Oatmeal', price: 14, imageSrc: `${process.env.PUBLIC_URL}/oatmeal.jpg` },
+        { name: 'Fresh Yogurt & Maple Granola', price: 13, description: '(Organic Berries, Raw New York Honey)', imageSrc: `${process.env.PUBLIC_URL}/van.jpg` },
         { name: 'Seasonal Fruit & Berries', price: 12, description: '(With Whipped Cream)' },
       ]
     },
-    // Add more categories here following the same format as above
+    {
+      category: 'EGGS', items: [
+        { name: 'Eggs Benedict', price: 16, description: '(Poached eggs, Canadian bacon, English muffin, hollandaise sauce)' },
+        { name: 'Omelette', price: 15, description: '(Choice of cheese, vegetables, and meat)' },
+        { name: 'Frittata', price: 14, description: '(Choice of vegetables and cheese)' },
+      ]
+    },
+    {
+      category: 'PANCAKES', items: [
+        { name: 'Classic Pancakes', price: 12, description: '(Buttermilk pancakes, maple syrup, whipped butter)' },
+        { name: 'Blueberry Pancakes', price: 14, description: '(Buttermilk pancakes, blueberries, maple syrup, whipped butter)' },
+        { name: 'Chocolate Chip Pancakes', price: 14, description: '(Buttermilk pancakes, chocolate chips, whipped butter)' },
+      ]
+    },
+    {
+      category: 'WAFFLES', items: [
+        { name: 'Belgian Waffles', price: 12, description: '(Golden brown waffles, maple syrup, whipped butter)' },
+        { name: 'Strawberry Waffles', price: 14, description: '(Golden brown waffles, fresh strawberries, whipped cream)' },
+        { name: 'Banana Nutella Waffles', price: 14, description: '(Golden brown waffles, sliced bananas, Nutella, whipped cream)' },
+      ]
+    },
+    {
+      category: 'BAGELS', items: [
+        { name: 'Smoked Salmon Bagel', price: 16, description: '(Toasted bagel, smoked salmon, cream cheese, capers, red onions, tomatoes)' },
+        { name: 'Vegetarian Bagel', price: 14, description: '(Toasted bagel, cream cheese, avocado, tomatoes, red onions)' },
+        { name: 'Egg and Cheese Bagel', price: 12, description: '(Toasted bagel, scrambled eggs, cheddar cheese)' },
+      ]
+    }
   ];
+
 
   return (
     <div>
@@ -114,21 +142,31 @@ const HomePage = () => {
       </section>
 
       <section className={styles.menu}>
-        <h2>Breakfast Menu</h2>
-        {menu.map((category, index) => (
-          <div key={index}>
-            <h3>{category.category}</h3>
-            <ul>
-              {category.items.map((item, i) => (
-                <li key={i}>
-                  {item.name} - ${item.price}
-                  {item.description && <span>({item.description})</span>}
-                </li>
-              ))}
-            </ul>
-          </div>
-        ))}
-      </section>
+  <h2>Breakfast Menu</h2>
+  <div className={styles.menuGrid}>
+    {menu.map((category, index) => (
+      <div key={index}>
+        <h3>{category.category}</h3>
+        <div className={styles.menuItems}>
+          {category.items.map((item, i) => (
+            <div key={i} className={styles.menuItem}>
+              <div className={styles.mimageContainer}>
+                <img src={item.imageSrc} alt={item.name} className={styles.menuImage} />
+              </div>
+              <div className={styles.menuItemInfo}>
+                <div className={styles.menuItemName}>{item.name}</div>
+                <div className={styles.menuItemDescription}>{item.description}</div>
+                <div className={styles.menuItemPrice}>${item.price}</div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    ))}
+  </div>
+</section>
+
+
 
       <section className={styles.description}>
         <p>
