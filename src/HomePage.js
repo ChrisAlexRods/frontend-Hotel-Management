69fb5ebed80484a66872fba608fc2f8840c7ad76
Christian Rodriguez <105233007+ChrisAlexRods@users.noreply.github.com>
2023-03-29 20:40:07 -0400

    pictures finished

--- src/HomePage.js
+++ src/HomePage.js
@@ -27,36 +27,36 @@ const HomePage = () => {
     {
       category: 'STARTERS', items: [
         { name: 'Old Fashioned Oatmeal', price: 14, imageSrc: `${process.env.PUBLIC_URL}/oatmeal.jpg` },
-        { name: 'Fresh Yogurt & Maple Granola', price: 13, description: '(Organic Berries, Raw New York Honey)', imageSrc: `${process.env.PUBLIC_URL}/van.jpg` },
-        { name: 'Seasonal Fruit & Berries', price: 12, description: '(With Whipped Cream)' },
+        { name: 'Fresh Yogurt & Maple Granola', price: 13, description: '(Organic Berries, Raw New York Honey)', imageSrc: `${process.env.PUBLIC_URL}/van.jpg`},
+        { name: 'Seasonal Fruit & Berries', price: 12, description: '(With Whipped Cream)', imageSrc: `${process.env.PUBLIC_URL}/sberry.jpg` },
       ]
     },
     {
       category: 'EGGS', items: [
-        { name: 'Eggs Benedict', price: 16, description: '(Poached eggs, Canadian bacon, English muffin, hollandaise sauce)', imageSrc: `${process.env.PUBLIC_URL}/ben.jpg` },
-        { name: 'Omelette', price: 15, description: '(Choice of cheese, vegetables, and meat)' },
-        { name: 'Frittata', price: 14, description: '(Choice of vegetables and cheese)' },
+        { name: 'Eggs Benedict', price: 16, description: '(Poached eggs, Canadian bacon, English muffin, hollandaise sauce)', imageSrc: `${process.env.PUBLIC_URL}/ben.jpg`},
+        { name: 'Omelette', price: 15, description: '(Choice of cheese, vegetables, and meat)', imageSrc: `${process.env.PUBLIC_URL}/oml.jpg`},
+        { name: 'Frittata', price: 14, description: '(Choice of vegetables and cheese)',imageSrc: `${process.env.PUBLIC_URL}/frit.jpeg`},
       ]
     },
     {
       category: 'PANCAKES', items: [
-        { name: 'Classic Pancakes', price: 12, description: '(Buttermilk pancakes, maple syrup, whipped butter)' },
-        { name: 'Blueberry Pancakes', price: 14, description: '(Buttermilk pancakes, blueberries, maple syrup, whipped butter)' },
-        { name: 'Chocolate Chip Pancakes', price: 14, description: '(Buttermilk pancakes, chocolate chips, whipped butter)' },
+        { name: 'Classic Pancakes', price: 12, description: '(Buttermilk pancakes, maple syrup, whipped butter)', imageSrc: `${process.env.PUBLIC_URL}/classic.jpg` },
+        { name: 'Blueberry Pancakes', price: 14, description: '(Buttermilk pancakes, blueberries, maple syrup, whipped butter)', imageSrc: `${process.env.PUBLIC_URL}/blue.jpg`},
+        { name: 'Chocolate Chip Pancakes', price: 14, description: '(Buttermilk pancakes, chocolate chips, whipped butter)', imageSrc: `${process.env.PUBLIC_URL}/choc.jpg`},
       ]
     },
     {
       category: 'WAFFLES', items: [
-        { name: 'Belgian Waffles', price: 12, description: '(Golden brown waffles, maple syrup, whipped butter)' },
-        { name: 'Strawberry Waffles', price: 14, description: '(Golden brown waffles, fresh strawberries, whipped cream)' },
-        { name: 'Banana Nutella Waffles', price: 14, description: '(Golden brown waffles, sliced bananas, Nutella, whipped cream)' },
+        { name: 'Belgian Waffles', price: 12, description: '(Golden brown waffles, maple syrup, whipped butter)', imageSrc: `${process.env.PUBLIC_URL}/bel.jpg`},
+        { name: 'Strawberry Waffles', price: 14, description: '(Golden brown waffles, fresh strawberries, whipped cream)', imageSrc: `${process.env.PUBLIC_URL}/straw.jpg`},
+        { name: 'Banana Nutella Waffles', price: 14, description: '(Golden brown waffles, sliced bananas, Nutella, whipped cream)', imageSrc: `${process.env.PUBLIC_URL}/nut.jpg`},
       ]
     },
     {
       category: 'BAGELS', items: [
-        { name: 'Smoked Salmon Bagel', price: 16, description: '(Toasted bagel, smoked salmon, cream cheese, capers, red onions, tomatoes)' },
-        { name: 'Vegetarian Bagel', price: 14, description: '(Toasted bagel, cream cheese, avocado, tomatoes, red onions)' },
-        { name: 'Egg and Cheese Bagel', price: 12, description: '(Toasted bagel, scrambled eggs, cheddar cheese)' },
+        { name: 'Smoked Salmon Bagel', price: 16, description: '(Toasted bagel, smoked salmon, cream cheese, capers, red onions, tomatoes)', imageSrc: `${process.env.PUBLIC_URL}/salmon.jpg` },
+        { name: 'Vegetarian Bagel', price: 14, description: '(Toasted bagel, cream cheese, avocado, tomatoes, red onions)', imageSrc: `${process.env.PUBLIC_URL}/veg.jpg` },
+        { name: 'Egg and Cheese Bagel', price: 12, description: '(Toasted bagel, scrambled eggs, cheddar cheese)', imageSrc: `${process.env.PUBLIC_URL}/ec.jpg` },
       ]
     }
   ];
